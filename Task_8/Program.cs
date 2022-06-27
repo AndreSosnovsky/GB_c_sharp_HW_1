dc5228bc77a011a6358e88ab6c023948b1f5a33c
AndreSosnovsky <andre.sosnovsky@gmail.com>
2022-06-27 18:35:21 +0300

    Задача 4

---
+++ Task_8/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Enter N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int notN = 2;
+while (notN <= N)
+{
+    Console.Write(notN);
+    Console.Write(", ");
+    notN = notN + 2;
+} 
