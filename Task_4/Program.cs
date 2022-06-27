c82e7db19e6a80b3f6e96dda74149d54620a62dc
AndreSosnovsky <andre.sosnovsky@gmail.com>
2022-06-27 18:34:31 +0300

    Задача 2

---
+++ Task_4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Enter number a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number b ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number c: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b > a)
+    max = b;
+if (c > max)
+    max = c;
+Console.WriteLine("Maximum is ");
+Console.WriteLine(max);
