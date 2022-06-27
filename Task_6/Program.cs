18dbb477fa7d5e473b0019732d641142498cfcda
AndreSosnovsky <andre.sosnovsky@gmail.com>
2022-06-27 18:35:00 +0300

    Задача 3

---
+++ Task_6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Enter number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a%2==0)
+{
+    Console.WriteLine("а is EVEN");
+}
+else
+{
+Console.WriteLine("a is ODD");
+}
