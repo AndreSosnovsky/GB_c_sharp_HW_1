1b92c2d4938e1c52b99236c521dcdec7281a61b3
AndreSosnovsky <andre.sosnovsky@gmail.com>
2022-06-27 18:33:48 +0300

    Задача 1 (2)

---
+++ Task_2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.Write("Enter number a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number b ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a == b)
+{
+    Console.WriteLine("The Numbers Are Equal");
+    return;
+}
+
+if (a > b)
+{
+    Console.WriteLine("Number a max");
+}
+else
+{
+    Console.WriteLine("Number b max");
+}
